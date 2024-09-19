59f155e27a99922ce77a47c682d00ea656cf8c22
mariaudovychenko <m.udovychenko@yahoo.com>
2024-09-19 02:42:15 +0200

    Worldwise with fake authentification

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// npm i
+// npm run dev
+// npm install eslint vite-plugin-eslint eslint-config-react-app --save-dev
